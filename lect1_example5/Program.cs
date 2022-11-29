d91bb3f3a4ae8ddbbcf6eae8a20cfcbb04d6fc4f
Natalia <dginni2009@gmail.com>
2022-11-29 20:16:43 +0300

    greeting myself

---
+++ lect1_example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter user's name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "natalia")
+{
+    Console.WriteLine("Hello, my beautiful lady!");
+}
+else
+{
+    Console.Write("Hello, you, ");
+    Console.Write(username);
+}
