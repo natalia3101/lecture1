bf6b0890a5c52955c5c083afa46538e4ae7ab4ee
Natalia <dginni2009@gmail.com>
2022-11-29 20:01:56 +0300

    random numbers

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿int number1 = 3;
-int number2 = 5;
+﻿int number1 = new Random().Next(1, 10); // 1, 2, 3 ...9 так как max не входит в диапазон 
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int result = number1 + number2;
 Console.WriteLine(result);
 
